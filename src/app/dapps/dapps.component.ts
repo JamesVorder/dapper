494bbd7be34e8345e37090b9c76ed55ec83c7f44
JamesVorder <james@bip44.email>
2019-01-21 12:03:02 -0500

    Added pagination logic to ts.

--- src/app/dapps/dapps.component.ts
+++ src/app/dapps/dapps.component.ts
@@ -1,26 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { DappsService } from '../services/dapps/dapps.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
-  selector: 'app-dapps',
-  templateUrl: './dapps.component.html',
-  styleUrls: ['./dapps.component.css']
+	selector: 'app-dapps',
+	templateUrl: './dapps.component.html',
+	styleUrls: ['./dapps.component.css']
 })
 export class DappsComponent implements OnInit {
 
-  dapps: Array<Object>
+	dapps: Array<Object> = new Array<Object>()
+	start: number
+	pageLength = 10
+	paginator: BehaviorSubject<number> = new BehaviorSubject<number>(0)
 
-  constructor(
-    public dappsService: DappsService
-  ) { 
-    this.dappsService.getDapps()
-    dappsService.dapps$.subscribe(newList => {
-      this.dapps = newList
-    })
-  }
+	constructor(
+		public dappsService: DappsService
+	) {  
+		this.dappsService.getDapps(this.start, this.pageLength)
+		dappsService.dapps$.subscribe(newList => {
+			this.dapps = newList
+		})
 
-  ngOnInit() {
-    
-  }
+		this.paginator.subscribe(newPage => {
+			this.start = newPage
+			//this.end = this.start + this.pageLength
+			this.dappsService.getDapps(this.start, this.pageLength)
+		})
+	}
+
+	public nextPage(){
+		this.paginator.next(this.start + this.pageLength)
+	}
+
+	public prevPage(){
+		let startIndex = this.start - this.pageLength
+		if(startIndex < 0){
+			this.paginator.next(0)
+		}
+		else{
+			this.paginator.next(startIndex)
+		}
+	}
+
+	/*public set pageLength(l: number){
+		this.pageLength = l
+		this.paginator.next(this.start)
+	}*/
+
+
+	ngOnInit() {
+
+	}
 
 }
--- src/app/services/dapps/dapps.service.ts
+++ src/app/services/dapps/dapps.service.ts
@@ -3,29 +3,29 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class DappsService {
 
-  dapps: BehaviorSubject<Array<Object>> = new BehaviorSubject<Array<Object>>([])
+	dapps: BehaviorSubject<Array<Object>> = new BehaviorSubject<Array<Object>>([])
 
-  constructor(
-    public http: HttpClient
-  ) { }
+	constructor(
+		public http: HttpClient
+	) { }
 
-  public getDapps(){
-    var url = 'https://194.1.237.94/GetDappList'
-    var queryString = 'StartNum=8&CountNum=10'
-    console.log("Getting " + url + "?" + queryString)
-    this.http.get(`${url}?${queryString}`, {responseType: 'json'}).toPromise().then(
-      (res) => {
-        console.log(res)
-        this.dapps.next(res['arr'])
-      }
-    )
-  }
+	public getDapps(start: number, count: number){
+		let url = 'https://194.1.237.94/GetDappList'
+		let queryString = `StartNum=${start}&CountNum=${count}`
+		console.log("Getting " + url + "?" + queryString)
+		this.http.get(`${url}?${queryString}`, {responseType: 'json'}).toPromise().then(
+			(res) => {
+				console.log(res)
+				this.dapps.next(res['arr'])
+			}
+		)
+	}
 
-  public get dapps$(): Observable<any[]> {
-    return this.dapps.asObservable()
-  }
+	public get dapps$(): Observable<any[]> {
+		return this.dapps.asObservable()
+	}
 }
